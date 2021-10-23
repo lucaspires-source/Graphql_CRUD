90da3a8d5c5204d1a9ca2e456669fb123dc1d8b0
lucaspires-source <pires.lucas94@gmail.com>
2021-10-23 12:27:36 -0300

    creating connection with mysql server

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,15 @@ import cors from 'cors'
 import { createConnection } from 'typeorm'
 
 const main = async () =>{
+await createConnection({
+    type:"mysql",
+    database:"graphqlcrud",
+    username:"root",
+    password:"123456",
+    logging:true,
+    synchronize:false,
+    entities:[]
+})    
 const app = express()
 app.use(cors())
 app.use(express.json())
