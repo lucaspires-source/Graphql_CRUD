51c60973d91bd9041b4be8bf834a2aa4f0a4f0ba
lucaspires-source <pires.lucas94@gmail.com>
2021-10-31 08:14:57 -0300

    basic structure on the front end form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("")
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
   const client = new ApolloClient ({
     uri:'http://localhost:3001/graphql',
     cache:new InMemoryCache(),
   })
   return (
     <ApolloProvider client={client}>
-      <div>oi</div>
+       <div className="createUser">
+        <input type="text" placeholder="name" onChange={(e) => {setName(e.target.value)}}/>
+        <input type="text" placeholder="username" onChange={(e) => {setUserName(e.target.value)}}/>
+        <input type="password" placeholder="password" onChange={(e) => {setPassword(e.target.value)}}/>
+        <button>Create User</button>
+       </div>
     </ApolloProvider>
 
   );
